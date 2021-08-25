b70656ad53c94879b719adb7d123af19d1260d37
zhangrengang <zhangrenang@ori-gene.cn>
2021-08-25 18:36:42 +0800

    update .gitignore

--- subphaser.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-config=sg.config
-genome=$1
-shift
-opts=$@
-pre=$(basename $(pwd))_
-python3 /share/home/nature/users/zhangrenang/subphaser/test_subphaser.py -i $genome -c $config $opts -pre $pre
